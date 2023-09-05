8f0fd38fda50f2525758d71945a91f842ed5bb02
Ezequiel <ki3lmigu3l@gmail.com>
2023-09-05 00:25:45 -0300

    Produto mapeado para a base de dados

---
+++ src/main/java/io/github/ki3lmigu3l/produtos/model/ProdutoModel.java
@@ -0,0 +1,43 @@
+package io.github.ki3lmigu3l.produtos.model;
+
+import jakarta.persistence.*;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.util.UUID;
+
+@Entity
+@Table(name = "TB_PRODUTO")
+public class ProdutoModel implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID idProduto;
+    private String nome;
+    private BigDecimal valor;
+
+    public UUID getIdProduto() {
+        return idProduto;
+    }
+
+    public void setIdProduto(UUID idProduto) {
+        this.idProduto = idProduto;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public BigDecimal getValor() {
+        return valor;
+    }
+
+    public void setValor(BigDecimal valor) {
+        this.valor = valor;
+    }
+}
