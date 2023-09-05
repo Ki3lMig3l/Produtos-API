dd5ccfba47b1e84555eb3965a2ee472916b4f79e
Ezequiel <ki3lmigu3l@gmail.com>
2023-09-05 00:11:17 -0300

    Primeiro Commit, Iniciando a API de produtos

---
+++ src/main/java/io/github/ki3lmigu3l/produtos/ProdutosApplication.java
@@ -0,0 +1,13 @@
+package io.github.ki3lmigu3l.produtos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProdutosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProdutosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/ki3lmigu3l/produtos/ProdutosApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.ki3lmigu3l.produtos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProdutosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
